c86fa2520dc25184e32c4cbb45121aa289a17f2b
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2023-03-15 00:28:53 -0300

    up

---
+++ Mundo 1/Ex02.py
@@ -0,0 +1,4 @@
+#Exercício Python 2: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
+nome = str(input('Digite Seu nome: '))
+print('Olá, Seja bem vinda {}'.format(nome))
