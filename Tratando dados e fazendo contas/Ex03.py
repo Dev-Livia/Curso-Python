03e9b816a362953f4037b02a7d41329f29d682b1
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2023-03-15 00:07:15 -0300

    ex03

---
+++ Tratando dados e fazendo contas/Ex03.py
@@ -0,0 +1,7 @@
+#Exercício Python 3: Crie um programa que leia dois números e mostre a soma entre eles.
+
+Num1 = int(input('Digite o Primeiro número: '))
+Num2 = int(input('Digite o Segundo número: '))
+soma = Num1 + Num2
+
+print('O resultado é {}'.format(soma))
