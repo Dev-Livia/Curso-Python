06f5f3bfd5e0b1e7d5443d1cf29c33c0744c1aa9
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2023-04-07 16:46:31 -0300

    Update Ex66.py

--- Mundo 2/Ex66.py
+++ Mundo 2/Ex66.py
@@ -1,12 +1,9 @@
 #Exercício Python 66: Crie um programa que leia números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre elas (desconsiderando o flag).
-cont = 1
-soma = 0
-while cont:
+cont = soma = 0
+while True:
     num = int(input('Digite um número (999 para para): '))
-    cont += 1
-    soma += num  
     if num == 999:
         break
-    cont -= 1
-soma -= 999 
+    soma += num
+    cont += 1 
 print(f'O total de números digitados foi {cont} e a soma entre esses números é {soma}!!!')
