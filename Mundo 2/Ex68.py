3f99ed18651b52fe66e6184b8330701632b9fe8c
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2023-04-09 02:35:58 -0300

    Create Ex68.py

---
+++ Mundo 2/Ex68.py
@@ -0,0 +1,32 @@
+#Exercício Python 68: Faça um programa que jogue par ou ímpar com o computador. O jogo só será interrompido quando o jogador perder, mostrando o total de vitórias consecutivas que ele conquistou no final do jogo.
+from random import randint
+
+print('^' * 20)
+print('VAMOS JOGAR PAR OU ÍMPAR')
+print('^' * 20)
+
+
+Vitoria = 0
+while True:
+    print('---' * 30)
+    Jogador = int(input('Digite um número: '))
+    pi = str(input('Par ou ímpar [P/Í]:  ')).upper().strip()
+    Computador = randint(0,101)
+    soma = (Jogador + Computador) 
+    rest = soma % 2
+    if rest == 0 and pi == 'P':
+        Vitoria += 1
+        print(f'Você escolheu {Jogador} e o computador {Computador}. Total de {soma} DEU PAR')
+        print('Você VENCEU')
+    elif rest != 0 and pi == 'P':
+        print(f'Você escolheu {Jogador} e o computador {Computador}. Total de {soma} DEU íMPAR')
+        print('VOCê PERDEU')
+    elif rest != 0 and pi == 'I':
+        Vitoria += 1
+        print(f'Você escolheu {Jogador} e o computador {Computador}. Total de {soma} DEU íMPAR')
+        print('VOCê VENCEU')
+    if rest == 0 and pi == 'I':
+        break
+print(f'Você escolheu {Jogador} e o computador {Computador}. Total de {soma} ')
+print(f'GAME OVER ! Você Venceu {Vitoria} Vezes')
+
