2f855c6297206f8bf63dbefcdb3a18a41d528033
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2023-04-28 13:27:42 -0300

    Update Ex77.py

--- Mundo 3/Ex77.py
+++ Mundo 3/Ex77.py
@@ -9,4 +9,4 @@ for p in palavras:
     print(f' \n Na palavra {p.upper()} temos ', end='')
     for letra in p:
         if letra.lower() in 'aeiou':
-            print(letra, end='')
+            print(letra, end=' ')
