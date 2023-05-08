549086e31615983ac3ba1e448d3f6352c4e1cae9
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2023-05-08 18:30:01 -0300

    Ex

---
+++ Mundo 1/Ex01.py
@@ -0,0 +1,6 @@
+#print('Olá, Mundo!')
+
+# ou podemos armazenar o texto em uma variavel como no exemplo a baixo
+
+msg = 'Olá, Mundo!!'
+print(msg)
---
+++ Mundo 1/Ex02.py
@@ -0,0 +1,4 @@
+#Exercício Python 2: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
+usuario = str(input('Digite seu nome: '))
+print('Olá, {} Seja Bem Vinda(o)'.format(usuario))
