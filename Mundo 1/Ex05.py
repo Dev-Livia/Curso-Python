115439e6d3803af63f9fd93bfbdb4a269e577aeb
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2023-05-08 22:59:19 -0300

    Create Ex05.py

---
+++ Mundo 1/Ex05.py
@@ -0,0 +1,5 @@
+#Exercício Python 5: Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+num = int(input('Digite um número: '))
+print('O Sucessor desse número é {}'.format(num + 1))
+print('O Antecessor desse número é {}'.format(num - 1))
