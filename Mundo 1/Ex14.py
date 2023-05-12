379e5bd8adca02efbfa9a645443d58ae6dbb27cf
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2023-05-12 15:05:01 -0300

    up

--- Mundo 1/Ex14.py
+++ Mundo 1/Ex14.py
@@ -1,5 +1,5 @@
 #Exercício Python 14: Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
 
 Tempreatura = float(input('Informe a temperatura: '))
-Conversão = 32 + (Tempreatura * 9 / 5)
+Conversão = (Tempreatura * 9 / 5) + 32 
 print('A temperatura de {}C° corresponde a {}°F'.format(Tempreatura, Conversão))
