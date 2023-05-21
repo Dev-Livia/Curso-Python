5c73f336c21ca4cd8c0725ff2fe77207138ba8f2
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2023-05-21 10:36:32 -0300

    Update Ex36.py

--- Mundo 2/Ex36.py
+++ Mundo 2/Ex36.py
@@ -1,12 +1,11 @@
 #Exercício Python 36: Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar. A prestação mensal não pode exceder 30% do salário ou então o empréstimo será negado.
-
-Valor_Casa = float(input('Qual o valor da casa que pretende comprar:R$ '))
-Salario = float(input('Quanto é seu salário?R$ '))
+Valor_Casa = float(input('Qual o valor da casa que deseja comprar?R$ '))
+Salario_Comprador = float(input('Qual o total que você ganha?R$ '))
 Anos = int(input('Em quantos anos deseja pagar? '))
 
-Prestação_Mensal = Valor_Casa / (Anos * 12)
+Prestacao_Mensal = Valor_Casa / (Anos * 12)
 
-if Prestação_Mensal <= Salario * 0.3:
-      print("Empréstimo aprovado! O valor da prestação mensal será de R$ {:.2f}".format(Prestação_Mensal))
+if Prestacao_Mensal <= Salario_Comprador * 0.3:
+    print('Empréstimo APROVADO! O valor da prestação será de {:.2f} '.format(Prestacao_Mensal))
 else:
-    print("Empréstimo negado. O valor da prestação excede 30% do salário.")
+    print('Emprestimo NEGADO! o valor da prestação excede 30% do salário.')
