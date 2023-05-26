d23b1d3863fa6b4ad3b3cd89494adae517145453
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2023-05-26 16:56:19 -0300

    Update Ex54.py

--- Mundo 2/Ex54.py
+++ Mundo 2/Ex54.py
@@ -1,1 +1,14 @@
 #Exercício Python 54: Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.
+from datetime import date
+contM = contMenor = 0
+for pessoas in range(1,8):
+    Dta_Nasc = int(input('Informe seu ano de nascimento: '))
+    Ano_Atual = date.today().year
+    Idade =  Ano_Atual - Dta_Nasc
+    if Idade >= 18:
+        contM += 1
+    else:
+        contMenor += 1
+print('===' * 20)
+print('O total de pessoas que atingiram a maior idade é {}'.format(contM))
+print('O total de pessoas que NÃO atingiram a maior idade é {}'.format(contMenor))
