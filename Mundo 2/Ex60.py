a26f87fbc064eb550fed9b914bdfc0764c0d9411
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2023-05-26 22:22:12 -0300

    Update Ex60.py

--- Mundo 2/Ex60.py
+++ Mundo 2/Ex60.py
@@ -2,15 +2,16 @@
 
 num = int(input("Digite um número: "))
 fatorial = 1
-contador = 1
+contador = num
 
 if num < 0:
     print("Não é possível calcular o fatorial de um número negativo.")
 elif num == 0:
     print("O fatorial de 0 é 1.")
 else:
-    while contador <= num:
+    while contador >= 1:
         fatorial *= contador
-        contador += 1
+        contador -= 1
 
     print("O fatorial de {} é {}.".format(num, fatorial))
+
