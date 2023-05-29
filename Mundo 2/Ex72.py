65949665c5a5f3922eff7ccc33a7b14919e3bf76
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2023-05-29 18:02:08 -0300

    update

---
+++ Mundo 2/Ex72.py
@@ -0,0 +1,9 @@
+#Exercício Python 72: Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+
+Contagem = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+while True:
+    Num = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= Num <= 20:
+        break
+    print('Tente novamente.', end='')
+print(f'Você digitou o número {Contagem[Num]}')
