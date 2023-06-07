0d09eb176f7000b3c6fb34c0be245bc36cc25ac8
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2023-06-07 14:17:36 -0300

    Update Ex78.py

--- Mundo 3/Ex78.py
+++ Mundo 3/Ex78.py
@@ -1,17 +1,17 @@
 #Exercício Python 078: Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.
 Lista = []
-Menor =  None
-Maior = 0
-for C in range(1,6):
+Menor = Maior =  None
+
+for C in range(0, 5):
     Num = int(input('Digite um número: '))
     Lista.append(Num)
     if Menor is None or Num < Menor:
         Menor = Num
-        Posição_Menor = Lista.index(Menor)
-    if Num > Maior:
+        Posição_Menor = C
+    if Maior is None or Num > Maior:
         Maior = Num
-        Posição_Maior = Lista.index(Maior)
+        Posição_Maior = C
 print(f' O menor número é: {Menor} na posição {Posição_Menor}')
-print('===' * 30)
+print('=' * 35)
 print(f'O maior número é: {Maior} na posição {Posição_Maior}')
 
